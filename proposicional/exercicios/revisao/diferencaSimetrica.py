27e0d4159f9b673f9652ab00192dae6c6ea358bc
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-07 18:16:17 -0300

    atualização

---
+++ proposicional/exercicios/revisao/diferencaSimetrica.py
@@ -0,0 +1,12 @@
+A = {1,2,3,4,5,6}
+B = {4,5,6,7,8,9,10}
+
+diferenca1 = A-B
+diferenca2 = B-A
+
+diferencaSimetrica = diferenca1.union(diferenca2)
+
+
+print(diferenca1)
+print(diferenca2)
+print(diferencaSimetrica)
