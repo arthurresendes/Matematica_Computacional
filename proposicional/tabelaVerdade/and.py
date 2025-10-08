176f461eff3c753d74587adec056dcc34c3834fc
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-08 14:32:49 -0300

    Atualizar

---
+++ proposicional/tabelaVerdade/and.py
@@ -0,0 +1,30 @@
+p = True
+q = True
+
+if p and q:
+    print(True)
+else:
+    print(False)
+
+p = True
+q = False
+
+if p and q:
+    print(True)
+else:
+    print(False)
+p = False
+q = True
+
+if p and q:
+    print(True)
+else:
+    print(False)
+
+p = False
+q = False
+
+if p and q:
+    print(True)
+else:
+    print(False)
