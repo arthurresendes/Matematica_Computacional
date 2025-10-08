6fe22385f7a170cfe4e3fa3ec7e198504a4a06bf
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-08 14:34:44 -0300

    Atualizar

---
+++ proposicional/tabelaVerdade/bicondicional.py
@@ -0,0 +1,31 @@
+p = True
+q = True
+
+if p == q:
+    print(True)
+else:
+    print(False)
+
+p = True
+q = False
+
+if p == q:
+    print(True)
+else:
+    print(False)
+
+p = False
+q = True
+
+if p == q:
+    print(True)
+else:
+    print(False)
+
+p = False
+q = False
+
+if p == q:
+    print(True)
+else:
+    print(False)
