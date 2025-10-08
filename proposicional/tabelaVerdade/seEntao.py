c78cf19d63887c707b0ca56e5ae01bf06dd39fda
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-08 14:33:55 -0300

    Atualizar

---
+++ proposicional/tabelaVerdade/seEntao.py
@@ -0,0 +1,31 @@
+p = True
+q = True
+
+if p == True and q == False:
+    print(False)
+else:
+    print(True)
+
+p = True
+q = False
+
+if p == True and q == False:
+    print(False)
+else:
+    print(True)
+
+p = False
+q = True
+
+if p == True and q == False:
+    print(False)
+else:
+    print(True)
+
+p = False
+q = False
+
+if p == True and q == False:
+    print(False)
+else:
+    print(True)
