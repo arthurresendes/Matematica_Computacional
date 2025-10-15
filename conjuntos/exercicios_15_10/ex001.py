ecaaffa22a51cb0f2d745ff1be2afde96636c48a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-15 19:47:19 -0300

    Atualizar

---
+++ conjuntos/exercicios_15_10/ex001.py
@@ -0,0 +1,14 @@
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7}
+
+# Calcule A ∪ B, A ∩ B, A − B e B − A.
+
+uniao = A.union(B)
+inteserc = A.intersection(B)
+a_sem_b = A-B
+b_sem_a = B-A
+
+print(uniao)
+print(inteserc)
+print(a_sem_b)
+print(b_sem_a)
