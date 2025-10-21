eee266dd8a5d775cd56e6a6c3b1b27da693cbcf2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-21 17:11:15 -0300

    Atualizar

---
+++ conjuntos/exercicios_semana20/ex003.py
@@ -0,0 +1,10 @@
+'''
+Determine o complementar de A em U = {1, 2, 3, 4, 5, 6, 7}, sendo A = {2, 4, 6}.
+'''
+
+U = {1, 2, 3, 4, 5, 6, 7}
+A = {2, 4, 6}
+
+complementar = U - A
+
+print(complementar)
