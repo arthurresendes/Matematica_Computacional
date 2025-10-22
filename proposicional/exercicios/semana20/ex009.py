b550809995879981a5ea24f3954774dc2d21a6b4
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-22 16:35:48 -0300

    Atualizar

---
+++ proposicional/exercicios/semana20/ex009.py
@@ -0,0 +1,14 @@
+p = True
+q = False
+r = False
+s = True
+
+if (p == q) and (r == s):
+    print(True)
+else:
+    print(False)
+   
+if (not p == q) and (not r == s):
+    print(True)
+else:
+    print(False)
