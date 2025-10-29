b7ed26599fa5870db97044dfe7dd5df0b08090b2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-29 15:22:26 -0300

    atualizar

---
+++ conjuntos/exercicios_semana20/ex010.py
@@ -0,0 +1,13 @@
+python_total = 50
+c_total = 32
+java_total = 26
+python_c_ambos = 10
+python_java_ambos = 12
+java_c_ambos = 15
+tudo_tres = 5
+total_pessoas = 115
+
+
+apenas_python_c = python_c_ambos - tudo_tres
+apenas_python_java = python_java_ambos - tudo_tres
+apenas_java_c = java_c_ambos - tudo_tres
