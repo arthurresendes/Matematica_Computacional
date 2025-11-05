e854ae5d612f758991f82cd51b3bc2746fb98e3f
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-05 17:35:25 -0300

    atualização

---
+++ funcoes_e_relacoes/par_ordenado/ex001.py
@@ -0,0 +1,19 @@
+'''
+Par ordenado é igual a (x,y) onde x é a abscissa e y é o ordenada
+(x,y) != (y,x)
+
+'''
+
+par1 = (1,2)
+par2 = (2,1)
+
+if par1==par2:
+    print(True)
+else:
+    print(False)
+
+par2 = (1,2)
+if par1==par2:
+    print(True)
+else:
+    print(False)
