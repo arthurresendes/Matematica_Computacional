12b45a6d5d6614641b02f35bcd5376bbdff88bec
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-05 18:00:52 -0300

    Atualizar

---
+++ funcoes_e_relacoes/relacao_binaria/ex001.py
@@ -0,0 +1,4 @@
+# Considere A = {0, 1, 2, 3}. Monte a relação “é menor que” dentro de A × A.
+
+rbAA = {(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)}
+print(f"A relação binaria entra AxA é menor que deu: {rbAA}")
