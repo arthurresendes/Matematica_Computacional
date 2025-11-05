cd47850ef228ce172be0441624467be33116da7c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-05 18:07:27 -0300

    Atualizar

---
+++ funcoes_e_relacoes/relacao_binaria/ex003.py
@@ -0,0 +1,4 @@
+# Monte uma relação R no conjunto A = {−2, −1, 0, 1, 2} definida por R = {(x, y) | y = |x|} (|-10| = 10 - Absoluto)
+
+r = {(-2,2),(-1,1),(0,0),(1,1),(2,2)}
+print(f"A relação binaria onde R = '{'(x, y) onde y = |x|'}' que deu: {r}")
