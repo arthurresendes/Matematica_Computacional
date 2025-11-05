8feb6a2ea24c5a648e943aa6af41d49befa98831
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-05 17:43:47 -0300

    Create ex005.py

---
+++ funcoes_e_relacoes/par_ordenado/ex005.py
@@ -0,0 +1,6 @@
+# Considere os pontos A(2, 3) e B(2, −3). O que eles têm em comum e o que muda?
+
+parA = (2,3)
+parB = (2,-3)
+
+print("Eles tem o valor de X em comum mas o de Y , sendo assim não pode ser considerado um par ordenado")
