f3a01dbd45257d5db05cb1fc4f7b203623d4baef
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-05 18:02:43 -0300

    Atualizar

---
+++ funcoes_e_relacoes/relacao_binaria/ex002.py
@@ -0,0 +1,7 @@
+# Se A = {1, 2, 3, 4} e B = {1, 4, 9, 16}, relacione os elementos por R = {(x, y) | y = x²}.
+
+a = {1,2,3,4}
+b = {1,4,9,16}
+
+r = {(1,1), (2,4), (3,9), (4,16)}
+print(f"A relação binaria entra AxB onde y = x² que deu: {r}")
