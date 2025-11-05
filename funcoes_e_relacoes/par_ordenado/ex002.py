3ac88e1ace9ed0598b1cde57dd3d4f1b804c3bc8
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-05 17:37:39 -0300

    Create ex002.py

---
+++ funcoes_e_relacoes/par_ordenado/ex002.py
@@ -0,0 +1,27 @@
+par1 = (1,'a')
+par2 = (2,'a')
+par3 = (1,'c')
+
+
+if par1 == par2:
+    print(True)
+else:
+    print(False)
+    
+if par1 == par3:
+    print(True)
+else:
+    print(False)
+
+par2 = (1,'a')
+par3 = (1,'a')
+
+if par1 == par2:
+    print(True)
+else:
+    print(False)
+    
+if par1 == par3:
+    print(True)
+else:
+    print(False)
