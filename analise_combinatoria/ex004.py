f5462b2bfdee25d79e6f7f00ba111fe2ac0a5988
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-18 19:25:10 -0300

    atualização

---
+++ analise_combinatoria/ex004.py
@@ -0,0 +1,7 @@
+'''
+4. Uma placa de identificação de usuário utiliza um código com 4 caracteres, sem repetir símbolos do
+conjunto {A, B, C, 1, 2}. Quantos códigos distintos podem ser formados?
+'''
+
+senhas =  5*4*3*2
+print(senhas)
