e8db1be0fd672f70fce8207c27b378b940b54fb6
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2025-11-19 17:36:23 -0300

    Criar ex006.py

---
+++ analise_combinatoria/ex006.py
@@ -0,0 +1,8 @@
+import math
+
+def fatorial(num):
+    return math.factorial(num)
+
+
+# De quantas maneiras 10 pessoas podem se organizar em uma fila?De quantas maneiras 10 pessoas podem se organizar em uma fila?
+print(fatorial(10))
