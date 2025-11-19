eb60757e48f493f254b6f7db2dd8b949ea6b9b52
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2025-11-19 17:35:09 -0300

    Criar ex005.py

---
+++ analise_combinatoria/ex005.py
@@ -0,0 +1,7 @@
+'''
+Senhas: Uma senha de 4 caracteres é formada por letras ou números, com o primeiro caractere sendo necessariamente uma letra.
+Quantas senhas podem ser criadas, sem repeticao?
+'''
+
+total = 26 * 35 * 34 * 33
+print(total)
