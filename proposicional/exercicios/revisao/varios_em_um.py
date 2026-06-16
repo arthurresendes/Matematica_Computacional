9b2bc65d1f536373155298fb3cd62b224b51f849
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-06-16 16:50:44 -0300

    Criar varios_em_um.py

---
+++ proposicional/exercicios/revisao/varios_em_um.py
@@ -0,0 +1,50 @@
+p =  input("Digite true ou false para p: ").lower()
+if p == 'true':
+    p = True
+else:
+    p = False
+
+q =  input("Digite true ou false para q: ").lower()
+if q == 'true':
+    q = True
+else:
+    q = False
+
+if p and q:
+    print("True , p e q não são verdadeiros")
+else:
+    print('False , p e q são diferentes')
+
+if p == True and q == False:
+    print("Se p entao q é falso")
+else:
+    print("True, p e q são V no se entao")
+
+if p == q:
+    print("E bicondicional")
+else:
+    print("Não é bicondicional")
+
+negacao_p = not p
+print(f"Ngeacao de p: {negacao_p}")
+
+r =  input("Digite true ou false para r: ").lower()
+if r == 'true':
+    r = True
+else:
+    r = False
+
+if (p and q) or not r == True:
+    print("True")
+else:
+    print("False")
+
+if not q == True and not p == False:
+    print("True")
+else:
+    print("False")
+
+if (p or q) and not (p and q) == True:
+    print("True")
+else:
+    print('False')
